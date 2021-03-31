75836e510a9aa9e59e950d0e7190de678212b61f
dennissio <di.nnissio@gmail.com>
2021-03-31 15:46:24 +0200

    keys

---
+++ app.py
@@ -0,0 +1,59 @@
+'''import libraries'''
+import streamlit as st
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import plotly.express as px
+
+
+
+
+
+
+
+
+'''1. Block (Janina)
+API anknüpfen (Python: json,...)
+Input des Nutzers einlesen
+Data handling (Python: pandas, numpy, ...) -> Alle Daten strukturiert eingelesen'''
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+'''2. Block (Deniz)
+Alle relevanten Darstellungen zu Temp (Python: seaborn, plotly, …)'''
+def run_app():
+    '''execute the whole program'''
+    def plot_temp(lat, lon, temp):
+        
+
+
+
+
+
+
+
+'''3. Block (Elena)
+Alle relevanten Darstellungen zu Nds (Python: seaborn, plotly, …)'''
+
+
+
+
+
+
+
+
+'''4. Block (Julian)
+Verbindung der relevanten Darstellungen zu Temp und Nds auf WebApp (Streamlit, HTML, CSS, Bootstrap, JS)'''
---
+++ keys.py
@@ -0,0 +1,3 @@
+'''keys for the API'''
+CLIMACELL_API = '722303f7admsh0e757fd7c14249cp107dacjsn0fbaa30a333d'
+MAPBOX_TOKEN = 'pk.eyJ1IjoiZGVubmlzc2lvIiwiYSI6ImNrbXg4NjhvZDBtOHkyb24xd3p5anE3NWYifQ.2U5ETPfl1WL1aGZFy5DZmA'
