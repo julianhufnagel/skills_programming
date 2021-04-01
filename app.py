bb83f685798c0daa62092dff5591f280adc3acb3
JaninaMoser <81177067+JaninaMoser@users.noreply.github.com>
2021-04-01 18:45:03 +0200

    Update app.py

--- app.py
+++ app.py
@@ -33,6 +33,28 @@ api_key = 'aad6e7a0184b7699b8dbd1f773f442d8'
 url = f'https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid={api_key}&units=metric'
 data = requests.get(url).json()
 
+#temperature information
+current.temperature = data['current']['temp']
+hourly.temperature = data['hourly']['temp']
+daily.temperature.day = data['daily']['temp']['day']
+daily.temperature.min = data['daily']['temp']['min']
+daily.temperature.max = data['daily']['temp']['max']
+daily.temperature.night = data['daily']['temp']['night']
+
+#weather information
+current.weather.description = data['current']['weather']['description']
+current.weather.icon = data['current']['weather']['icon']
+hourly.weather.description = data['hourly']['weather']['description']
+hourly.weather.description = data['hourly']['weather']['icon']
+daily.weather.description = data['daily']['weather']['description']
+daily.weather.icon = data['daily']['weather']['icon']
+
+#rain information
+minutely.precipitation = data['minutely']['precipitation']
+current.rain = data['current']['rain']
+hourly.rain = data['hourly']['rain']
+daily.rain = data['daily']['rain']
+
 
 
 
