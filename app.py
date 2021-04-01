73b70789f8fc0a741207527b5884dd4cc2b4272a
JaninaMoser <81177067+JaninaMoser@users.noreply.github.com>
2021-04-01 18:17:53 +0200

    Update app.py

--- app.py
+++ app.py
@@ -17,6 +17,22 @@ API anknüpfen (Python: json,...)
 Input des Nutzers einlesen
 Data handling (Python: pandas, numpy, ...) -> Alle Daten strukturiert eingelesen'''
 
+import geopy
+from geopy.geocoders import Nominatim
+
+city = input("Please enter a city: ")
+address='city'
+geolocator = Nominatim(user_agent="Your_Name")
+location = geolocator.geocode(address)
+
+lat = location.latitude
+lon = location.longitude
+
+import requests
+api_key = 'aad6e7a0184b7699b8dbd1f773f442d8'
+url = f'https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid={api_key}&units=metric'
+data = requests.get(url).json()
+
 
 
 
